ba580fb636321e06aff5136ee90de7a7bc1f97f8
Jesper Thörnberg (jeth11) <jesper.thornberg@teliacompany.com>
2020-08-13 02:28:49 +0200

    test

---
+++ src/components/game.js
@@ -0,0 +1,170 @@
+export const questions = [
+    {
+      index: 1,
+      guess: null,
+      facit: null,
+      locked: false,
+      completed: false,
+      allPlayersReady: false
+    },
+    {
+      index: 2,
+      guess: null,
+      facit: null,
+      locked: false,
+      completed: false,
+      allPlayersReady: false
+    },
+/*     {
+      index: 3,
+      guess: null,
+      facit: null,
+      locked: false,
+      completed: false,
+      allPlayersReady: false
+    },
+    {
+      index: 4,
+      guess: null,
+      facit: null,
+      locked: false,
+      completed: false,
+      allPlayersReady: false
+    },
+    {
+      index: 5,
+      guess: null,
+      facit: null,
+      locked: false,
+      completed: false,
+      allPlayersReady: false
+    },
+    {
+      index: 6,
+      guess: null,
+      facit: null,
+      locked: false,
+      completed: false,
+      allPlayersReady: false
+    },
+    {
+      index: 7,
+      guess: null,
+      facit: null,
+      locked: false,
+      completed: false,
+      allPlayersReady: false
+    },
+    {
+      index: 8,
+      guess: null,
+      facit: null,
+      locked: false,
+      completed: false,
+      allPlayersReady: false
+    },
+    {
+      index: 9,
+      guess: null,
+      facit: null,
+      locked: false,
+      completed: false,
+      allPlayersReady: false
+    },
+    {
+      index: 10,
+      guess: null,
+      facit: null,
+      locked: false,
+      completed: false,
+      allPlayersReady: false
+    },
+    {
+      index: 11,
+      guess: null,
+      facit: null,
+      locked: false,
+      completed: false,
+      allPlayersReady: false
+    },
+    {
+      index: 12,
+      guess: null,
+      facit: null,
+      locked: false,
+      completed: false,
+      allPlayersReady: false
+    },
+    {
+      index: 13,
+      guess: null,
+      facit: null,
+      locked: false,
+      completed: false,
+      allPlayersReady: false
+    },
+    {
+      index: 14,
+      guess: null,
+      facit: null,
+      locked: false,
+      completed: false,
+      allPlayersReady: false
+    },
+    {
+      index: 15,
+      guess: null,
+      facit: null,
+      locked: false,
+      completed: false,
+      allPlayersReady: false
+    },
+    {
+      index: 16,
+      guess: null,
+      facit: null,
+      locked: false,
+      completed: false,
+      allPlayersReady: false
+    },
+    {
+      index: 17,
+      guess: null,
+      facit: null,
+      locked: false,
+      completed: false,
+      allPlayersReady: false
+    },
+    {
+      index: 18,
+      guess: null,
+      facit: null,
+      locked: false,
+      completed: false,
+      allPlayersReady: false
+    },
+    {
+      index: 19,
+      guess: null,
+      facit: null,
+      locked: false,
+      completed: false,
+      allPlayersReady: false
+    },
+    {
+      index: 20,
+      guess: null,
+      facit: null,
+      locked: false,
+      completed: false,
+      allPlayersReady: false
+    },
+    {
+      index: 21,
+      guess: null,
+      facit: null,
+      locked: false,
+      completed: false,
+      allPlayersReady: false
+    } */
+  ];
