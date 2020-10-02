b7f2c5d4dd9d958d4e5b08d6ec0495824c99d5ed
jeth318 <jesper.thornberg@me.com>
2020-10-02 14:40:23 +0200

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,23 @@
 #!/usr/bin/env bash
-
-BRANCH_NAME=$1;
-
-
-if [[ $BRANCH_NAME == "master" ]]; then
-    cd /home/pi/Apps/zero-to-hundred/client
-else
-    exit 0;
-fi
-
-echo "Stashing lock files "
+cd /home/pi/Apps/zero-to-hundred/client
+echo "Stashing lock files"
 git stash
 
 echo "Dropping stash"
 git stash drop
 
-echo "Pulling from ${BRANCH_NAME}"
-git pull origin ${BRANCH_NAME}
-echo "Pulled successfully"
+echo "Pulling from Master"
+git pull origin master
+echo "Pulled successfully from master"
 
-echo "Removing node_modules and re-installing dependencies"
+echo "Installing dependencies..."
+rm -rf node_modules
 npm install
-echo "Project dependencies was installed"
+echo "Dependencies were installed"
+
 echo "Rebuilding application"
 npm run build
+
 echo "Rebuild OK"
 echo "Deployment complete, and it was a success!"
-echo "Version ${NEW_VERSION} is now live"
 exit 0
