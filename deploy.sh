ab5f18c191bb26180a4d5a592895cfdb3f9110e8
jeth318 <jesper.thornberg@me.com>
2022-07-04 10:19:57 +0200

    Revert a little

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ if [[ $branch == "main" ]]; then
     echo "On main branch. Will build and ship to server"
     npm run build
     echo "Shipping dist to server"
-    rsync -arvz -e 'ssh -p 2244' --progress $(pwd)/dist/* jeth@ton-618.ddns.net:/home/jeth/apps/zero-to-hundred
+    rsync -arvz -e 'ssh -p 2244' --progress $(pwd)/dist/* jeth@pipher.local:/var/www/zero-to-hundred
 else
     echo "Not on main branch. Skipping build and deploy."
 fi
