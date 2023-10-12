d04479c5e97b89385d7812245fc5225a5efb47aa
jeth318 <jesper.thornberg@me.com>
2023-10-13 00:18:58 +0200

    Update endpoint

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import styleImport, { VantResolve } from 'vite-plugin-style-import';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import styleImport, { VantResolve } from "vite-plugin-style-import";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +8,7 @@ export default defineConfig({
     vue(),
     styleImport({
       resolves: [VantResolve()],
-    }),],
-    base: '/zero-to-hundred/'
-})
+    }),
+  ],
+  base: "/",
+});
